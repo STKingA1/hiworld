dec54de264245148389daf409b51ab353e162aaa
wangxc <wangxc0726@outlook.com>
2020-06-21 14:23:53 +0800

    test

--- src/hiWord.java
+++ src/hiWord.java
@@ -2,5 +2,6 @@ public class hiWord {
     public static void main(String[] args) {
         System.out.println("hi word");
         System.out.println("test 2");
+        System.out.println("test 2");
     }
 }
